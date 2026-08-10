d5d41861b9fb22a57e176ac744716103164ce14a
ivanimmanuel-dev <ivanimmanuel1234@gmail.com>
2026-08-10 19:42:58 -0400

    Implemented KNNClassifier

---
+++ Project_OOP/KNNClassifier.cpp
@@ -0,0 +1,10 @@
+#include "KNNClassifier.h"
+#include <iostream>
+
+using namespace std;
+
+int KNNClassifier::classify(const Data& data) const
+{
+    cout << "KNNClassifier::classify()" << endl;
+    return -1;
+}
