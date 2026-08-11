b1c607f2df338c92290b34fde6d1456bd5d6320c
MiguelTAR <mtarazona4489@conestogac.on.ca>
2026-08-10 22:58:18 -0400

    Request

--- Project_OOP/NNClassifier.cpp
+++ Project_OOP/NNClassifier.cpp
@@ -8,40 +8,28 @@
 
 using namespace std;
 
-NNClassifier::NNClassifier(const vector<Data>& trainingData) 
+// Constructor that initializes the classifier with training data
+NNClassifier::NNClassifier(const vector<Data>& trainingData)
 {
-	// Constructor that initializes the classifier with training data
-	this->trainingData = trainingData;
+	this->trainingData = trainingData;						// Stores the training data in the classifier
 }
 
+// Function to classify a new data point using the nearest neighbor algorithm
 int NNClassifier::classify(const Data& data) const
 {
-	double mini_distance = INFINITY; // Initialize to a large value
-	int nearest_label = -1;			// Initialize to an invalid label
-
+	double min_distance = INFINITY;							// Initializes to a large value
+	int nearest_label = -1;									// Initializes to an invalid label
 
 	// For loop to find the nearest neighbor
 	for (const Data& item : trainingData)
 	{
-		double distance = sqrt(pow(item.getX() - data.getX(), 2) + pow(item.getY() - data.getY(), 2) + pow(item.getZ() - data.getZ(), 2));
-		if (distance < mini_distance)
-		{
-			mini_distance = distance;
-			nearest_label = item.getLabel();
-			for (auto item : trainingData) {
-				double distance = sqrt(
-					pow(item.x - data.x, 2) +
-					pow(item.y - data.y, 2) +
-					pow(item.z - data.z, 2)
-				);
+		double distance = sqrt(pow(item.getX() - data.getX(), 2) + pow(item.getY() - data.getY(), 2) + pow(item.getZ() - data.getZ(), 2));	// Calculates the Euclidean distance between the new data point and the training data point
 
-				// Update the nearest label if a closer neighbor is found
-				if (distance < mini_distance) {
-					mini_distance = distance;
-					nearest_label = item.label;
-				}
-			}
-			return nearest_label;
+		if (distance < min_distance)						// If the calculated distance is less than the current minimum distance, update the minimum distance and nearest label
+		{
+			min_distance = distance;
+			nearest_label = item.getLabel();
 		}
 	}
+	return nearest_label;									// Returns the label of the nearest neighbor
 }
